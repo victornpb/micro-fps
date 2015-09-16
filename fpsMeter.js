cc687ee3e637d1cee52c06e9ce8d465297f958d2
victornpb <victornpb@gmail.com>
2015-09-16 11:36:15 -0300

    implemented jitter calculation

--- fpsMeter.js
+++ fpsMeter.js
@@ -1,38 +1,95 @@
 /**
-* FPS Meter
-* @author Victor N - www.vitim.us
-*
-* @param {function} callback Function that will display the fps rate
-* @param {number} refreshRate Defines the frequency the fps will be updated in ms
-* @returns {function}
-*/
-function fpsMeter(callback, refreshRate){
+ * This callback is displayed as a global member.
+ * @callback fpsCallback
+ * @param {number} fps The current frames per second
+ * @this {Object} this Object containig other values, like average jitter, timestamps, etc
+ */
+
+
+
+/**
+ * FPS Meter - Returns a function that is used to compute the framerate without the overhead of updating the DOM every frame.
+ * @author Victor B - www.vitim.us
+ * @param {callback} fpsCallback Function called on every DOM update
+ * @param {number} refresh Updates per second of the DOM
+ * @param {element} elm DOM Element to write the framerate
+ * @return {function} Returns a function that will be called inside the loop
+ */
+function fpsMeter(fpsCallback, refresh, elm){
+    //var elm;             //element
+    //var refresh;         //refresh every x seconds
     
-    if(typeof callback!="function") throw new Error("callback must be a function");
+    var frameCount = 0,    //number of frames since last computation
+        trigger = 1;       //compute frame rate every `x` frames (calculated on the go)
     
-    var compRate = 1;   //compute frame rate every x frames (calculated on the go)
-    var frames = 0;     //number of frames since last timing
-    var last = 0;       //used for timing
-    refreshRate/=1000;
+    var prev = 0; //previous timestamp
+    
+    var prevFps = 0; //previous fps value
+    var jitter = 0;  //computed jitter (exp avg)
     
     return function(){
-        if(++frames > compRate){
-            var now = Date.now();
-            var diff = now - last;
+        if(++frameCount > trigger){
             
-            if(diff>0){
-                var fps = (1000/(diff/frames))<<0;
-                last = now;
-                frames = 0;
+            var now = Date.now();
+            if(prev===0) prev = now;
+            var elapsed = now - prev;
+            
+            if(elapsed>0){
+                //compute fps
+                var fps = (1000/(elapsed/frameCount))<<0;
                 
-                //exponential ramp the next update to match the current refresh rate
-                compRate = ((compRate*0.5)+((fps*refreshRate)*0.5));
-                 
-                var timestamp = now;
-                var currentRefreshRate = diff;
-                callback(fps, currentRefreshRate, timestamp, arguments);
+                //compute jitter
+                jitter = (jitter+Math.abs(prevFps-fps))/2;
+                
+                //move the trigger value exponentialy to match the current refresh rate.
+                trigger = ((trigger*0.5)+((fps*refresh)*0.5))<<0;
+                
+                //dispatch callback
+                if(fpsCallback){
+                    fpsCallback.call({ 
+                        fps: fps,
+                        jitter: jitter,
+                        now: now,
+                        previousTimestamp: prev,
+                        elapsed: elapsed,
+                        frameCount: frameCount,
+                        triggerRate: trigger,  
+                    }, fps);
+                }
+                if(elm) elm.innerHTML = fps;
+                
+                prev = now;
+                prevFps = fps;
+                frameCount = 0;
+                
+                return true;
             }
-            else compRate*=2;
+            else{
+                trigger *= 2;                
+            }
         }
     }
 }
+
+/* Example
+    
+    var f = fpsMeter(null, 4, fpsDiv);
+    
+    //or
+    
+    var f = fpsMeter(function(fps){
+        
+        console.log("FPS:"+this.fps+" Jitter:"+this.jitter);
+        
+        fpsDiv.innerHTML = fps;
+    }, 4);
+    
+    
+    //game loop
+    setInterval(function(){
+    
+        f();
+        //do stuff
+    
+    }, 1);
+*/
