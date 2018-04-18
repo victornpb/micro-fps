935dbdaa5c052c211e27ac2b80f6db530a0a0fed
victornpb <victornpb@gmail.com>
2018-04-18 00:32:10 -0300

    added tests

--- __tests__/test.js
+++ __tests__/test.js
@@ -1,9 +1,140 @@
 import microFps from '../src/index';
 
-describe(`my first test`, () => {
-  it(`must return a function`, () => {
-    function cb(){}
-    const fpsTick = microFps(cb);
-    expect(fpsTick).toBeDefined();
+/**
+ * Mocking performance.now() method
+ * @class MockPerformanceNow
+ */
+class MockPerformanceNow {
+  constructor() {
+    const self = this;
+
+    this._original = global.performance;
+    this.timestamp = 1E8;
+
+    global.performance = {
+      now() {
+        return self.timestamp;
+      },
+    };
+  }
+  restore() {
+    global.performance = this._original;
+  }
+}
+
+const mockPerf = new MockPerformanceNow();
+
+describe('validation', () => {
+
+  it(`should require a callback`, () => {
+    expect(() => {
+      const fpsTick = microFps();
+    }).toThrowError();
+  });
+
+
+  it(`should require refresh rate to be a number`, () => {
+    const mockCallback = jest.fn();
+
+    expect(microFps(mockCallback)).toBeTruthy();
+    expect(microFps(mockCallback), 1).toBeTruthy();
+    expect(microFps(mockCallback), 0).toBeTruthy();
+    expect(microFps(mockCallback), 10).toBeTruthy();
+    expect(microFps(mockCallback), 0.5).toBeTruthy();
+
+    expect(() => {
+      const fpsTick = microFps(mockCallback, -1);
+    }).toThrowError();
+
+    expect(() => {      const mockCallback = jest.fn();
+      const fpsTick = microFps(mockCallback, NaN);
+    }).toThrowError();
+
+    expect(() => {
+      const fpsTick = microFps(mockCallback, null);
+    }).toThrowError();
+
+    expect(() => {
+      const fpsTick = microFps(mockCallback, {});
+    }).toThrowError();
+
+    expect(() => {
+      const fpsTick = microFps(mockCallback, Infinity);
+    }).toThrowError();
+
+    expect(() => {
+      const fpsTick = microFps(mockCallback, -Infinity);
+    }).toThrowError();
+
+    expect(() => {
+      const fpsTick = microFps(mockCallback, "foo");
+    }).toThrowError();
   });
 });
+
+describe('fps calculation', () => {
+  it('Should be 50fps on the 3rd frame, refresh rate set to 0', (done) => {
+    expect.assertions(1);
+
+    mockPerf.timestamp = 1E8;
+
+    const FPS = 50;
+    const FRAMEDURATION = 1000 / FPS;
+
+    const fpsTick = microFps((info) => {
+      expect(info.fps).toBe(FPS);
+      done();
+    }, 0);
+
+
+    fpsTick();
+    mockPerf.timestamp += FRAMEDURATION; // increment in 20ms (50fps)
+    fpsTick();
+    mockPerf.timestamp += FRAMEDURATION; // increment in 20ms (50fps)
+    fpsTick();
+  });
+
+  // it(`should be 50fps`, (done) => {
+  //   mockPerf.timestamp = 1;
+
+  //   const fpsTick = microFps((info) => {
+  //     expect(info.fps).toEqual(50);
+  //     done();
+  //   }, 0);
+
+  //   fpsTick();
+  //   mockPerf.timestamp = 1000/50;
+  //   fpsTick();
+  // });
+
+
+  // it(`should be 50fps 2`, (done) => {
+  //   expect.assertions(1);
+
+  //   mockPerf.timestamp = 1E5;
+
+  //   const FPS = 50;
+  //   const DURATION = 2000;
+  //   const FRAMEDURATION = 1000 / FPS;
+
+  //   const fpsTick = microFps((info) => {
+  //     // if (mockPerf.timestamp < 1000) return;
+  //     // expect(info.fps).toBeGreaterThanOrEqual(FPS - 1);
+  //     // expect(info.fps).toBeLessThanOrEqual(FPS + 1);
+
+  //     // done();
+  //   }, 0);
+
+
+  //   for (let i = 0; i < (DURATION / FRAMEDURATION); i++) {
+  //     fpsTick();
+  //     mockPerf.timestamp += FRAMEDURATION; // increment in 20ms (50fps)
+  //     console.log(mockPerf.timestamp);
+  //   }
+  // });
+
+
+  
+});
+
+
